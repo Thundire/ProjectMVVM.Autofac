49635c8d3a378755eaf51aff27c4bbcb3155c455
Tarxos <ftarxos@gmail.com>
2021-06-14 19:15:55 +0300

    add extension to ContainerBuilder to register Regions service, change name of view handler registration method

--- Thundire.MVVM.WPF.Autofac/AutofacContainerExtensions.cs
+++ Thundire.MVVM.WPF.Autofac/AutofacContainerExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Autofac;
 using Autofac.Builder;
+using Thundire.MVVM.WPF.Services;
 using Thundire.MVVM.WPF.Services.Interfaces;
+using Thundire.MVVM.WPF.Services.Regions;
 using Thundire.MVVM.WPF.Services.ViewService;
 using Thundire.MVVM.WPF.Services.ViewService.Interfaces;
 using Thundire.MVVM.WPF.Services.ViewService.Models;
@@ -10,7 +12,7 @@ namespace Thundire.MVVM.WPF.Autofac
 {
     public static class AutofacContainerExtensions
     {
-        public static void AddViewHandlerCache( this ContainerBuilder builder, Action<IViewRegister> registration) 
+        public static void AddViewHandlerService(this ContainerBuilder builder, Action<IViewRegister> registration) 
         {
             var viewRegister = new ViewRegister(new AutofacContainerBuilder(builder));
             registration?.Invoke(viewRegister);
@@ -19,6 +21,15 @@ namespace Thundire.MVVM.WPF.Autofac
             builder.RegisterType<AutofacContainer>().As<IDIContainer>().SingleInstance();
             builder.RegisterInstance(viewRegister).As<IViewRegisterCache>();
             builder.RegisterType<ViewHandlerService>().As<IViewHandlerService>();
+        }
+
+        public static void AddRegionsService(this ContainerBuilder builder, Action<ITemplatesCacheBuilder> configuration)
+        {
+            ITemplatesRegister register = new DataTemplatesRegister();
+            register.AddTemplates(configuration);
+
+            builder.RegisterInstance(register).As<ITemplatesCache>();
+            builder.RegisterType<RegionsService>();
         }
 
         public static void SetLifeTimeMode<TImplementer>(
